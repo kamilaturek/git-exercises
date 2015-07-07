fba0bbe3b532ec9481277759bdfa3008e9ebe9eb
kamilaturek <kamila.turek@proacta.pl>
2015-07-08 01:35:24 +0200

    przygotowanie prostej aplikacji Spring Boot + Angular JS

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Krótka zabawa z branchami");
+        return model;
+    }
+    
+}
