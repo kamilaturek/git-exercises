fbb8378e0d27a7bacb8cb9f792b97d364204bcd6
dominikat <dominika.targonska@gmail.com>
2015-07-09 10:40:58 +0200

    czesc frontedowa

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,5 +2,11 @@ angular.module('hello', [])
     .controller('home', function($scope, $http) {
         $http.get('/resource/').success(function(data) {
             $scope.greeting = data;
-        })
+        });
+
+        $scope.getToday = function(){
+            $http.get('/today/').success(function(data) {
+                $scope.today = data;
+            });
+        };
     });
